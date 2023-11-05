42ea1700757c9010e0b7786eb46772e4328dd85e
niszco <nico.marszalek@gmail.com>
2023-11-05 15:56:24 +0100

    Code cleanup

--- src/scripts/SceneManager.js
+++ src/scripts/SceneManager.js
@@ -1,9 +1,14 @@
 import * as THREE from "three";
-import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 
-// Cube :
+const gameWindow = document.getElementById('game-renderer');
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+gameWindow.appendChild( renderer.domElement );
+
+// Cube : (utilisé pour le prototype)
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
@@ -19,84 +24,61 @@ const FOV = 75;
 const SCREEN_ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT;
 const NEAR = 1;
 const FAR = 1000;
-const MIN_CAMERA_RADIUS = 2;
-const MAX_CAMERA_RADIUS = 150;
 const camera = new THREE.PerspectiveCamera(FOV, SCREEN_ASPECT, NEAR, FAR);
 
-// Listener de la molette de la souris pour le zoom de la caméra
-
-const SCROLL_MOUSE_UP = 0;
-const SCROLL_MOUSE_DOWN = 1;
-let isScrollUp = false;
-let isScrollDown = false;
-
-
-let cameraOrigin = new THREE.Vector2();
-let cameraRadius = camera.position.z;
-
-
+const MIN_CAMERA_POSITION = 2;
+const DEFAULT_CAMERA_POSITION = camera.position.z = 5;
+const MAX_CAMERA_POSITION = 100;
 
 scene.add(camera);
 
-// Gestion du zoom avec la molette de la souris
+// Gestion du zoom avec la molette de la souris avec listener de la molette de la souris pour le zoom de la caméra.
+
 document.addEventListener('wheel', onZoom);
 
-function onZoom(event) {
+function onZoom(e) {
 	const zoomSpeed = 0.01;
-  
-	camera.position.z -= event.deltaY * zoomSpeed;
-	camera.position.z = Math.min(MAX_CAMERA_RADIUS, Math.max(MIN_CAMERA_RADIUS, camera.position.z));
+	DEFAULT_CAMERA_POSITION;
+	camera.position.z -= e.deltaY * zoomSpeed;
+	camera.position.z = Math.min(MAX_CAMERA_POSITION, Math.max(MIN_CAMERA_POSITION, camera.position.z));
 	camera.updateProjectionMatrix();
   }
 
 // Fin camera
 
-const gameWindow = document.getElementById('game-renderer');
+// Player mouvement controls :
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-gameWindow.appendChild( renderer.domElement );
+const mouvementControlsWASD = ['w', 'a', 's', 'd'];
+const mouvementControlsZQSD = ['z', 'q', 's', 'd']; // Pour les clavier FR AZERTY
 
-camera.position.z = 5;
+function playerMouvementControls() {
+	// Trouver comment obtenir le layout du clavier afin de permettre au joueurs de se déplacer (pour l'instant que en QWERTY)
+	document.addEventListener('keydown', (e) => {
+		switch(e.key) {
+			case mouvementControlsWASD[0]:
+				cube.position.y += 0.3
+				//camera.position.y += 0.3 déplace la caméra avec le joueur : besoin d'un arrière plan pour l'utiliser
+			break;
+			case mouvementControlsWASD[1]:
+				cube.position.x -= 0.3
+				//camera.position.x -= 0.3
+			break;
+			case mouvementControlsWASD[2]:
+				cube.position.y -= 0.3
+				//camera.position.y -= 0.3
+			break;
+			case mouvementControlsWASD[3]:
+				cube.position.x += 0.3
+				//camera.position.x += 0.3
+			break;
+		}
+	})
+}
 
 function animate() {
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 }
 animate();
-
-// Player mouvment controls :
-
-const movementControlsWASD = ['w', 'a', 's', 'd'];
-const mouvementControlsZQSD = ['z', 'q', 's', 'd'];
-
-function playerMouvementControls() {
-	// Trouver comment obtenir le layout du clavier afin de permettre au joueurs de se déplacer (pour l'instant que en QWERTY)
-	document.addEventListener('keydown', (e) => {
-		switch(e.key) {
-			case movementControlsWASD[0]:
-				cube.position.y += 0.3
-				//camera.position.y += 0.3 déplace la caméra avec le joueur : besoin d'un arrière plan pour le tester
-			break;
-			case movementControlsWASD[1]:
-				cube.position.x -= 0.3
-				//camera.position.x -= 0.3
-			break;
-			case movementControlsWASD[2]:
-				cube.position.y -= 0.3
-				//camera.position.y -= 0.3
-			break;
-			case movementControlsWASD[3]:
-				cube.position.x += 0.3
-				//camera.position.x += 0.3
-			break;
-		}
-	})
-
-	document.addEventListener('keyup', (e) => {
-
-	})
-}
-
 playerMouvementControls();
 onZoom();
